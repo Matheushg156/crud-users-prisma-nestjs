54727fa522c537c1cf722efde339dd9ed99bcec3
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2023-10-29 12:44:22 -0300

    Configura o ThrottlerModule no app.module para proteger todas as rotas da API

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 100
+    }]),
+    UserModule,
+    AuthModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
+  exports: [AppService],
 })
 export class AppModule {}

4af050245c509c9403ff72d69bdb2f1cfa8d4471
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2023-10-29 17:35:29 -0300

    Usa validator nativo do nest para files

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { BadRequestException, Body, Controller, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors
+} from '@nestjs/common';
 import { AuthLoginDTO } from './dto/auth-login.dto';
 import { AuthRegisterDTO } from './dto/auth-register.dto';
 import { AuthForgotPasswordDTO } from './dto/auth-forgot-password.dto';
@@ -7,7 +19,7 @@ import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { User } from 'src/decorators/user.decoretor';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileFieldsInterceptor, FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { join } from 'path';
 import { FileService } from 'src/file/file.service';
 
@@ -48,8 +60,16 @@ export class AuthController {
 
   @UseInterceptors(FileInterceptor('file'))
   @UseGuards(AuthGuard)
-  @Post('send-photo')
-  async uploadFile(@User() user, @UploadedFile() file: Express.Multer.File) {
+  @Post('send-file')
+  async uploadFile(
+    @User() user,
+    @UploadedFile(new ParseFilePipe({
+      validators: [
+        new FileTypeValidator({ fileType: 'image/*' }),
+        new MaxFileSizeValidator({ maxSize: 1024 * 50 })
+      ]
+    })) file: Express.Multer.File
+  ) {
 
     const path = join(__dirname, '..', '..', 'storage', 'photos', `photo-${user.id}.jpeg`)
 
@@ -60,5 +80,31 @@ export class AuthController {
     } catch (error) {
       throw new BadRequestException(error.message);
     }
+  }
+
+  // Rota para enviar vários arquivos
+  @UseInterceptors(FilesInterceptor('files'))
+  @UseGuards(AuthGuard)
+  @Post('send-files')
+  async uploadFiles(@User() user, @UploadedFiles() files: Express.Multer.File[]) {
+
+    return files
+
+  }
+
+  // Rota para enviar vários arquivos com campos específicos
+  @UseInterceptors(FileFieldsInterceptor([{
+    name: 'photo',
+    maxCount: 1
+  }, {
+    name: 'documents',
+    maxCount: 10
+  }]))
+  @UseGuards(AuthGuard)
+  @Post('send-files-fields')
+  async uploadFilesFields(@User() user, @UploadedFiles() files: { photo: Express.Multer.File, documents: Express.Multer.File[] }) {
+
+    return { user, files }
+
   }
 }
